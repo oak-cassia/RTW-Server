b4906fe9515bb699428e83cc3d583f6c0e926562
oak-cassia <gnlckswjd1@gmail.com>
2024-11-18 20:52:32 +0900

    refactor: 비동기 함수명 끝에 Async추가, query 가독성 향상

--- RTWWebServer/Database/Repository/AccountRepository.cs
+++ RTWWebServer/Database/Repository/AccountRepository.cs
@@ -6,84 +6,72 @@ namespace RTWWebServer.Database.Repository;
 
 public class AccountRepository(MySqlConnection connection) : IAccountRepository
 {
-    private readonly MySqlConnection _connection = connection;
-
-    public async Task<Account?> FindById(int id)
+    public async Task<Account?> FindByIdAsync(int id)
     {
-        try
-        {
-            string query = $"SELECT * FROM Account WHERE id = @{nameof(id)}";
-            using var command = new MySqlCommand(query, _connection);
-            command.Parameters.AddWithValue("@nameof(id)", id);
+        string query = $"""
+                        SELECT *
+                        FROM Account 
+                        WHERE id = @{nameof(id)}
+                        """;
+        using var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue($"@{nameof(id)}", id);
 
-            using var reader = await command.ExecuteReaderAsync();
-            if (await reader.ReadAsync())
-            {
-                return new Account
-                (
-                    reader.GetInt32("Id"),
-                    reader.GetString("UserName"),
-                    reader.GetString("Email"),
-                    reader.GetString("Password"),
-                    reader.GetString("Salt")
-                );
-            }
-        }
-        catch (Exception ex)
+        using var reader = await command.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
         {
-            Console.WriteLine($"Error in FindById: {ex.Message}");
+            return new Account
+            (
+                reader.GetInt32("Id"),
+                reader.GetString("UserName"),
+                reader.GetString("Email"),
+                reader.GetString("Password"),
+                reader.GetString("Salt")
+            );
         }
 
         return null;
     }
 
-    public async Task<Account?> FindByEmail(string email)
+    public async Task<Account?> FindByEmailAsync(string email)
     {
-        try
-        {
-            string query = $"SELECT * FROM Account WHERE email = @{nameof(email)}";
-            await using var command = new MySqlCommand(query, _connection);
-            command.Parameters.AddWithValue($"@{nameof(email)}", email);
+        string query = $"""
+                        SELECT *
+                        FROM Account 
+                        WHERE email = @{nameof(email)} 
+                        """;
 
-            await using var reader = await command.ExecuteReaderAsync();
-            if (await reader.ReadAsync())
-            {
-                return new Account
-                (
-                    reader.GetInt32("Id"),
-                    reader.GetString("UserName"),
-                    reader.GetString("Email"),
-                    reader.GetString("Password"),
-                    reader.GetString("Salt")
-                );
-            }
-        }
-        catch (Exception ex)
+        await using var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue($"@{nameof(email)}", email);
+
+        await using var reader = await command.ExecuteReaderAsync();
+        if (await reader.ReadAsync())
         {
-            Console.WriteLine($"Error in FindByEmail: {ex.Message}");
+            return new Account
+            (
+                reader.GetInt32("Id"),
+                reader.GetString("UserName"),
+                reader.GetString("Email"),
+                reader.GetString("Password"),
+                reader.GetString("Salt")
+            );
         }
 
         return null;
     }
 
-    public Task<WebServerErrorCode> CreateAccount(string username, string email, string password, string salt)
+    public Task<WebServerErrorCode> CreateAccountAsync(string username, string email, string password, string salt)
     {
-        try
-        {
-            string query = "INSERT INTO Account (username, email, password, salt) VALUES (@username, @email, @password, @salt)";
-            using var command = new MySqlCommand(query, _connection);
-            command.Parameters.AddWithValue("@username", username);
-            command.Parameters.AddWithValue("@email", email);
-            command.Parameters.AddWithValue("@password", password);
-            command.Parameters.AddWithValue("@salt", salt);
+        string query = $"""
+                        INSERT INTO Account (UserName, Email, Password, Salt)
+                        VALUES (@{nameof(username)}, @{nameof(email)}, @{nameof(password)}, @{nameof(salt)})
+                        """;
+        using var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue($"@{nameof(username)}", username);
+        command.Parameters.AddWithValue($"@{nameof(email)}", email);
+        command.Parameters.AddWithValue($"@{nameof(password)}", password);
+        command.Parameters.AddWithValue($"@{nameof(salt)}", salt);
 
-            command.ExecuteNonQuery();
-            return Task.FromResult(WebServerErrorCode.Success);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error in CreateAccount: {ex.Message}");
-            return Task.FromResult(WebServerErrorCode.InternalServerError);
-        }
+        command.ExecuteNonQuery();
+        return Task.FromResult(WebServerErrorCode.Success);
     }
 }
