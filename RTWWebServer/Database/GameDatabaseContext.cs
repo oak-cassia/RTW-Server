92ea1ed3c5462352d834aaf6553396a57196e2e4
oak-cassia <52772732+oak-cassia@users.noreply.github.com>
2024-11-30 18:50:23 +0900

    Webserver 코드 포맷팅 #ATS-22

--- RTWWebServer/Database/GameDatabaseContext.cs
+++ RTWWebServer/Database/GameDatabaseContext.cs
@@ -3,4 +3,5 @@ using RTWWebServer.Configuration;
 
 namespace RTWWebServer.Database;
 
-public class GameDatabaseContext(IOptions<DatabaseConfiguration> configuration) : BaseDatabaseContext(configuration.Value.GameDatabase);
+public class GameDatabaseContext(IOptions<DatabaseConfiguration> configuration)
+    : BaseDatabaseContext(configuration.Value.GameDatabase);
