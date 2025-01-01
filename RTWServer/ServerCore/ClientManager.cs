cd266c00f9d55001d935ac89b5014cd376f1fa04
oak_cassia <gnlckswjd1@gmail.com>
2025-01-01 23:23:33 +0900

    feat add: 세션 관리할 ClientManager 추가 - send, receive 스레드가 동일하지 않아서 연결이 끊긴 경우 동기화 목적
    임시 id

---
+++ RTWServer/ServerCore/ClientManager.cs
@@ -0,0 +1,28 @@
+using System.Collections.Concurrent;
+
+namespace RTWServer.ServerCore;
+
+public class ClientManager
+{
+    private ConcurrentDictionary<string, IClient> _clients = new ConcurrentDictionary<string, IClient>();
+    
+    public void AddClient(IClient client)
+    {
+        _clients[client.Id] = client;
+    }
+    
+    public void RemoveClient(IClient client)
+    {
+        _clients.TryRemove(client.Id, out _);
+    }
+    
+    public IClient? GetClient(string id)
+    {
+        return _clients.GetValueOrDefault(id);
+    }
+    
+    public IEnumerable<IClient> GetAllClients()
+    {
+        return _clients.Values;
+    }
+}
