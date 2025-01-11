2e65ba674434df0cba9a60eb03790c735810bf43
oak_cassia <gnlckswjd1@gmail.com>
2025-01-12 01:40:54 +0900

    feat add: ClientSession Send 기능

--- RTWServer/ServerCore/implementation/ClientSession.cs
+++ RTWServer/ServerCore/implementation/ClientSession.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using RTWServer.ServerCore.Interface;
 
 namespace RTWServer.ServerCore.implementation;
@@ -13,6 +14,11 @@ public class ClientSession
     private readonly IPacketHandler _packetHandler;
     private readonly IPacketFactory _packetFactory;
 
+    private readonly ConcurrentQueue<IPacket> _sendQueue = new();
+    private readonly Lock _sendLock = new();
+
+    private bool _isSending;
+
     public string Id { get; private init; }
 
     public ClientSession(IClient client, IPacketHandler packetHandler, IPacketFactory packetFactory, string id)
@@ -21,6 +27,7 @@ public class ClientSession
         _packetHandler = packetHandler;
         _packetFactory = packetFactory;
         Id = id;
+        _isSending = false;
     }
 
     public async Task StartSessionAsync(CancellationToken token)
@@ -45,6 +52,13 @@ public class ClientSession
             Console.WriteLine(e);
             throw;
         }
+    }
+
+    public async Task SendAsync(IPacket packet)
+    {
+        _sendQueue.Enqueue(packet);
+        
+        await FlushSendQueueAsync();
     }
 
     private async Task<IPacket?> ReadPacketAsync(byte[] buffer)
@@ -73,7 +87,7 @@ public class ClientSession
         // 패킷 생성
         var packetId = BitConverter.ToInt32(buffer, HEADER_PACKET_ID_OFFSET);
         var payload = new ReadOnlyMemory<byte>(buffer, HEADER_SIZE, payloadLength);
-        
+
         return _packetFactory.CreatePacket(packetId, payload);
     }
 
@@ -99,6 +113,65 @@ public class ClientSession
 
         return true;
     }
+
+    private async Task FlushSendQueueAsync()
+    {
+        lock (_sendLock)
+        {
+            if (_isSending)
+            {
+                return;
+            }
+
+            _isSending = true;
+        }
+
+        while (true)
+        {
+            if (!_sendQueue.TryDequeue(out var packet))
+            {
+                lock (_sendLock)
+                {
+                    // 한번 더 확인하는 이유 - 아래 조건문이 없는 경우
+                    // 1. 현재 스레드: Dequeue 실패,lock 대기
+                    // 2. 다른 스레드: Enqueue 후 첫번째 lock 획득, isSending 이 참이므로 반환
+                    // 3. 현재 스레드: lock 획득, isSending = false 후 반환
+                    // 2번에서 Enqueue한 패킷 기아
+                    if (!_sendQueue.TryDequeue(out packet))
+                    {
+                        _isSending = false;
+                        return;
+                    }
+                }
+            }
+
+            try
+            {
+                await SendPacketAsync(packet);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+
+    private async Task SendPacketAsync(IPacket packet)
+    {
+        // TODO: 직렬화를 미리 끝내면 전송 간의 시간 차이가 줄어들어 성능 향상
+        var payload = packet.Serialize();
+
+        var packetIdLength = BitConverter.GetBytes((int)packet.PacketId);
+        var payloadLength = BitConverter.GetBytes(payload.Length);
+
+        var sendBuffer = new byte[HEADER_SIZE + payload.Length];
+        Buffer.BlockCopy(packetIdLength, 0, sendBuffer, HEADER_PACKET_ID_OFFSET, sizeof(int));
+        Buffer.BlockCopy(payloadLength, 0, sendBuffer, HEADER_LENGTH_OFFSET, sizeof(int));
+        Buffer.BlockCopy(payload, 0, sendBuffer, HEADER_SIZE, payload.Length);
+
+        await _client.SendAsync(sendBuffer);
+    }
+
 
     public void Disconnect()
     {
