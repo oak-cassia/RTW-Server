547d1f7b11a70e91aec5a7bd0745538ea07f14cf
oak_cassia <gnlckswjd1@gmail.com>
2025-01-20 22:26:48 +0900

    feat add: PipeWriter 사용하여 네트워크 버퍼에 write

--- RTWServer/ServerCore/implementation/TcpClientImpl.cs
+++ RTWServer/ServerCore/implementation/TcpClientImpl.cs
@@ -3,28 +3,23 @@ using RTWServer.ServerCore.Interface;
 
 namespace RTWServer.ServerCore.implementation;
 
-public class TcpClientImpl : IClient
+public class TcpClientImpl(TcpClient client) : IClient
 {
-    private readonly TcpClient _client;
-    
-    public TcpClientImpl(TcpClient client)
-    {
-        _client = client;
-    }
-    
-    public bool IsConnected => _client.Connected;
+    public Stream Stream { get; } = client.GetStream();
+    public bool IsConnected => client.Connected;
+
     public async Task SendAsync(byte[] buffer)
     {
-        await _client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+        await client.GetStream().WriteAsync(buffer, 0, buffer.Length);
     }
 
     public async Task<int> ReceiveAsync(byte[] buffer, int offset, int length)
     {
-        return await _client.GetStream().ReadAsync(buffer, offset, length); 
+        return await client.GetStream().ReadAsync(buffer, offset, length);
     }
 
     public void Close()
     {
-        _client.Close();
+        client.Close();
     }
 }
