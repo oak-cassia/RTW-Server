08a31c598231d04059e058a062b667759798a563
oak_cassia <gnlckswjd1@gmail.com>
2025-01-27 22:44:35 +0900

    feat add: IClientSession 추가

--- RTWServer/ServerCore/implementation/ClientSessionManager.cs
+++ RTWServer/ServerCore/implementation/ClientSessionManager.cs
@@ -1,29 +1,30 @@
 using System.Collections.Concurrent;
+using RTWServer.ServerCore.Interface;
 
 namespace RTWServer.ServerCore.implementation;
 
 public class ClientSessionManager
 {
-    private ConcurrentDictionary<string, ClientSession> _clientSessions = new ConcurrentDictionary<string, ClientSession>();
+    private ConcurrentDictionary<string, IClientSession> _clientSessions = new ();
     
-    public void AddClientSession(ClientSession clientSession)
+    public void AddClientSession(IClientSession clientSession)
     {
         _clientSessions[clientSession.Id] = clientSession;
     }
     
-    public void RemoveClientSession(ClientSession clientSession)
+    public void RemoveClientSession(IClientSession clientSession)
     {
         _clientSessions.TryRemove(clientSession.Id, out _);
         
         clientSession.Disconnect();
     }
     
-    public ClientSession? GetClientSession(string id)
+    public IClientSession? GetClientSession(string id)
     {
         return _clientSessions.GetValueOrDefault(id);
     }
     
-    public IEnumerable<ClientSession> GetAllClientSessions()
+    public IEnumerable<IClientSession> GetAllClientSessions()
     {
         return _clientSessions.Values;
     }
