39ff2d6e0d6edc1326e35c78ca3d5adad6ec371c
oak_cassia <gnlckswjd1@gmail.com>
2025-01-29 15:00:06 +0900

    feat add: 네트워크 송/수신 중 연결 해제 시 처리

--- RTWServer/ServerCore/implementation/ClientSessionManager.cs
+++ RTWServer/ServerCore/implementation/ClientSessionManager.cs
@@ -5,25 +5,23 @@ namespace RTWServer.ServerCore.implementation;
 
 public class ClientSessionManager : IClientSessionManager
 {
-    private ConcurrentDictionary<string, IClientSession> _clientSessions = new ();
-    
+    private ConcurrentDictionary<string, IClientSession> _clientSessions = new();
+
     public void AddClientSession(IClientSession clientSession)
     {
         _clientSessions[clientSession.Id] = clientSession;
     }
-    
-    public void RemoveClientSession(IClientSession clientSession)
+
+    public void RemoveClientSession(string id)
     {
-        _clientSessions.TryRemove(clientSession.Id, out _);
-        
-        clientSession.Disconnect();
+        _clientSessions.TryRemove(id, out _);
     }
-    
+
     public IClientSession? GetClientSession(string id)
     {
         return _clientSessions.GetValueOrDefault(id);
     }
-    
+
     public IEnumerable<IClientSession> GetAllClientSessions()
     {
         return _clientSessions.Values;
