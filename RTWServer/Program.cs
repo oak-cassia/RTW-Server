0eaade952335491dad82534d2b02ff9e51a3670a
oak_cassia <gnlckswjd1@gmail.com>
2025-05-17 21:25:09 +0900

    refactor: ClientSession에 유저 상태 추가하고 UserState 삭제

--- RTWServer/Program.cs
+++ RTWServer/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Net;
 using Microsoft.Extensions.Logging;
-using RTWServer.Auth;
 using RTWServer.Game;
 using RTWServer.Packet;
 using RTWServer.ServerCore.implementation;
+using RTWServer.ServerCore.Interface;
 
 // TODO : 설정 파일에서 IP 주소와 포트 번호를 읽어와서 사용하도록 수정
 string ipAddress = "127.0.0.1";
@@ -22,16 +22,16 @@ try
     IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
 
     GamePacketFactory packetFactory = new GamePacketFactory();
-    
-    // Create the user state manager
-    IUserStateManager userStateManager = new UserStateManager(loggerFactory);
+
+    // ClientSessionManager now requires ILoggerFactory
+    IClientSessionManager clientSessionManager = new ClientSessionManager(loggerFactory); 
 
     AsyncAwaitServer server = new AsyncAwaitServer(
         new TcpServerListener(endpoint, loggerFactory),
-        new GamePacketHandler(loggerFactory, userStateManager),
+        new GamePacketHandler(loggerFactory),
         loggerFactory,
         new PacketSerializer(packetFactory),
-        new ClientSessionManager()
+        clientSessionManager // Pass the initialized clientSessionManager
     );
 
     logger.LogInformation("Server running at {IpAddress}:{Port}", ipAddress, port);
--- RTWServer/ServerCore/Interface/IClientSession.cs
+++ RTWServer/ServerCore/Interface/IClientSession.cs
@@ -2,9 +2,14 @@ namespace RTWServer.ServerCore.Interface;
 
 public interface IClientSession
 {
-    string Id { get; }
+    string Id { get; } // Session ID, will also serve as Player ID after authentication
+    string? AuthToken { get; } 
+    bool IsAuthenticated { get; } 
     
     Task StartSessionAsync(CancellationToken token);
     
     Task SendAsync(IPacket packet);
+
+    // PlayerId in the return tuple now refers to the authenticated Session ID (or a mapping if needed)
+    Task<(NetworkDefinition.ErrorCode.RTWErrorCode ErrorCode, int PlayerId)> ValidateAuthTokenAsync(string authToken); 
 }
