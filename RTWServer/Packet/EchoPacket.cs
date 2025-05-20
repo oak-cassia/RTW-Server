421aacbc918e78370618bca6f22eeb5797cc303b
oak_cassia <gnlckswjd1@gmail.com>
2025-05-20 23:53:56 +0900

    Revert "feat add: BasePacket 클래스 추가 및 EchoPacket 클래스 수정"
    This reverts commit 261f53b8a10f845efe17a11e03d793a95b8e1f89.

--- RTWServer/Packet/EchoPacket.cs
+++ RTWServer/Packet/EchoPacket.cs
@@ -1,16 +1,26 @@
 using RTWServer.Enum;
+using RTWServer.ServerCore.Interface;
 
 namespace RTWServer.Packet;
 
-public class EchoPacket : BasePacket
+public class EchoPacket : IPacket
 {
-    public EchoPacket(PacketId packetId, byte[] payload) 
-        : base(packetId, payload)
+    public PacketId PacketId { get; }
+    public byte[] Payload { get; }
+
+    public EchoPacket(PacketId packetId, byte[] payload)
     {
+        PacketId = packetId;
+        Payload = payload;
     }
-    
-    public EchoPacket(byte[] payload)
-        : base(PacketId.EchoTest, payload)
+
+    public int GetPayloadSize()
     {
+        return Payload.Length;
+    }
+
+    public void WriteToBuffer(Span<byte> buffer)
+    {
+        Payload.CopyTo(buffer);
     }
 }
