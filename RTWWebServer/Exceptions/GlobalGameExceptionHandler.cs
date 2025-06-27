488406282d90cd4d6c5bbf89564b2763faf94cf2
oak_cassia <gnlckswjd1@gmail.com>
2025-06-28 02:26:57 +0900

    feat: 예외처리 중앙화

---
+++ RTWWebServer/Exceptions/GlobalGameExceptionHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NetworkDefinition.ErrorCode;
+using RTWWebServer.DTO;
+using RTWWebServer.Exceptions;
+
+namespace RTWWebServer.Exceptions
+{
+    public class GlobalGameExceptionHandler(ILogger<GlobalGameExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            GameResponse<object> response;
+
+            if (exception is GameException gameException)
+            {
+                logger.LogInformation(
+                    "game exception: {ErrorCode}, message: {Message}",
+                    gameException.ErrorCode, gameException.Message);
+
+                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                response = GameResponse<object>.Fail(gameException.ErrorCode);
+            }
+            else
+            {
+                // 기타 예외는 서버 오류로 간주
+                logger.LogError(exception, "unhandled exception: {Message}", exception.Message);
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                response = GameResponse<object>.Fail(WebServerErrorCode.InternalServerError);
+            }
+
+            await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+            return true;
+        }
+    }
+}
