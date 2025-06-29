ffb559fb52da24cb64aca8968adc2ba44aeca0a4
oak_cassia <gnlckswjd1@gmail.com>
2025-06-30 01:16:03 +0900

    refactor: GamePacketHandler에서 ProtoPacket 의존 분리

--- RTWServer/Game/Packet/GamePacketFactory.cs
+++ RTWServer/Game/Packet/GamePacketFactory.cs
@@ -1,6 +1,7 @@
 using RTW.NetworkDefinition.Proto.Packet;
 using RTWServer.Packet;
 using RTWServer.ServerCore.Interface;
+using Google.Protobuf;
 
 namespace RTWServer.Game.Packet;
 
@@ -17,5 +18,11 @@ public class GamePacketFactory : IPacketFactory
             PacketId.SAuthResult => new ProtoPacket(packetId, SAuthResult.Parser.ParseFrom(payloadBytes)),
             _ => throw new ArgumentOutOfRangeException(nameof(packetId), packetId, null)
         };
+    }
+
+    public IPacket CreatePacket(int packetIdNum, IMessage message)
+    {
+        PacketId packetId = (PacketId)packetIdNum;
+        return new ProtoPacket(packetId, message);
     }
 }
