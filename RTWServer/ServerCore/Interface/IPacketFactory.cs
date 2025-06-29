aed88bdad0155c287219c36a06f907eea5473fa7
oak_cassia <gnlckswjd1@gmail.com>
2025-06-30 01:16:03 +0900

    refactor: GamePacketHandler에서 ProtoPacket 의존 분리

--- RTWServer/ServerCore/Interface/IPacketFactory.cs
+++ RTWServer/ServerCore/Interface/IPacketFactory.cs
@@ -1,6 +1,9 @@
+using Google.Protobuf;
+
 namespace RTWServer.ServerCore.Interface;
 
 public interface IPacketFactory
 {
     IPacket CreatePacket(int packetId, ReadOnlySpan<byte> payload);
+    IPacket CreatePacket(int packetId, IMessage message);
 }
