4506e3943eb53516e600779c1db346a55b50626a
oak_cassia <gnlckswjd1@gmail.com>
2025-07-01 23:01:08 +0900

    refactor: 예외 처리 중앙화에 맞게 수정

--- RTWWebServer/Service/IAccountService.cs
+++ RTWWebServer/Service/IAccountService.cs
@@ -2,6 +2,6 @@ namespace RTWWebServer.Service;
 
 public interface IAccountService
 {
-    Task<bool> CreateAccountAsync(string userName, string email, string password);
+    Task CreateAccountAsync(string userName, string email, string password);
     Task<string> CreateGuestAccountAsync();
 }
