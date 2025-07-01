bccc83ff3f17c2c261982cf723aa5c38c8874069
oak_cassia <gnlckswjd1@gmail.com>
2025-07-02 00:49:05 +0900

    fix: GameException from abstract to concrete class

--- RTWWebServer/Exceptions/GameExceptions.cs
+++ RTWWebServer/Exceptions/GameExceptions.cs
@@ -2,7 +2,7 @@ using NetworkDefinition.ErrorCode;
 
 namespace RTWWebServer.Exceptions;
 
-public abstract class GameException(string message, WebServerErrorCode errorCode) : Exception(message)
+public class GameException(string message, WebServerErrorCode errorCode) : Exception(message)
 {
     public WebServerErrorCode ErrorCode { get; } = errorCode;
 }
