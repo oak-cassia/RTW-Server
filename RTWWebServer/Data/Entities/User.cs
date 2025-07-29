a493ed3c8f68130bfc0fa8196a9e9a9d36e4b889
oak_cassia <gnlckswjd1@gmail.com>
2025-07-30 01:37:35 +0900

    feat: 게임 로직 추가를 위해 User 수정

--- RTWWebServer/Data/Entities/User.cs
+++ RTWWebServer/Data/Entities/User.cs
@@ -1,14 +1,24 @@
 namespace RTWWebServer.Data.Entities;
 
-public class User(long id, string? guid, string? email, int userType, string? nickname, DateTime createdAt, DateTime updatedAt)
+public class User(
+    long id, 
+    string? guid, 
+    string? email, 
+    int userType, 
+    string? nickname, 
+    int level,
+    long currentExp,
+    int currentStamina,
+    int maxStamina,
+    DateTime? lastStaminaRecharge,
+    long premiumCurrency,
+    long freeCurrency,
+    long? mainCharacterId,
+    DateTime createdAt, 
+    DateTime updatedAt)
 {
     private User()
-        : this(0, string.Empty, string.Empty, 0, string.Empty, default, default)
-    {
-    }
-
-    public User(string? guid, string? email, int userType, string? nickname, DateTime createdAt, DateTime updatedAt)
-        : this(0, guid, email, userType, nickname, createdAt, updatedAt)
+        : this(0, string.Empty, string.Empty, 0, string.Empty, 0, 0, 0, 0, DateTime.UtcNow, 0, 0, null, default, default)
     {
     }
 
@@ -17,6 +27,14 @@ public class User(long id, string? guid, string? email, int userType, string? ni
     public string? Email { get; set; } = email;
     public int UserType { get; set; } = userType;
     public string? Nickname { get; set; } = nickname;
+    public int Level { get; set; } = level;
+    public long CurrentExp { get; set; } = currentExp;
+    public int CurrentStamina { get; set; } = currentStamina;
+    public int MaxStamina { get; set; } = maxStamina;
+    public DateTime LastStaminaRecharge { get; set; } = lastStaminaRecharge ?? DateTime.UtcNow;
+    public long PremiumCurrency { get; set; } = premiumCurrency;
+    public long FreeCurrency { get; set; } = freeCurrency;
+    public long? MainCharacterId { get; set; } = mainCharacterId;
     public DateTime CreatedAt { get; set; } = createdAt;
     public DateTime UpdatedAt { get; set; } = updatedAt;
 }
