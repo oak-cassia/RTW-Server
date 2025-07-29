a1d14b7aa7212a3d20f442489b418038c6dcab65
oak_cassia <gnlckswjd1@gmail.com>
2025-07-29 22:45:56 +0900

    feat: 데이터가 더 중요한 request class들 Record로 변환

--- RTWWebServer/DTOs/Request/UpdateNicknameRequest.cs
+++ RTWWebServer/DTOs/Request/UpdateNicknameRequest.cs
@@ -1,7 +1,3 @@
 namespace RTWWebServer.DTOs.Request;
 
-public class UpdateNicknameRequest
-{
-    public long UserId { get; set; }
-    public string Nickname { get; set; } = string.Empty;
-}
+public record UpdateNicknameRequest(long UserId, string Nickname);
