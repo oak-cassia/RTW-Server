c98b73956f4626e8ddf4b373042caa4b2ada3e73
oak_cassia <gnlckswjd1@gmail.com>
2025-08-01 22:51:39 +0900

    feat: asp.net의 Authentication 사용

--- RTWWebServer/Controllers/GameController.cs
+++ RTWWebServer/Controllers/GameController.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using RTWWebServer.DTOs;
-using RTWWebServer.DTOs.Request;
 using RTWWebServer.DTOs.Response;
 using RTWWebServer.Services;
+using NetworkDefinition.ErrorCode;
 
 namespace RTWWebServer.Controllers;
 
@@ -11,9 +12,18 @@ namespace RTWWebServer.Controllers;
 public class GameController(IGameEntryService gameEntryService) : ControllerBase
 {
     [HttpPost("enter")]
-    public async Task<GameResponse<UserSession>> EnterGame([FromBody] GameEntryRequest request)
+    [Authorize]
+    public async Task<GameResponse<UserSession>> EnterGame()
     {
-        var userSession = await gameEntryService.EnterGameAsync(request.JwtToken);
+        var jwtToken = HttpContext.Request.Headers["Authorization"]
+            .FirstOrDefault()?.Split(" ").Last();
+            
+        if (string.IsNullOrEmpty(jwtToken))
+        {
+            return GameResponse<UserSession>.Fail(WebServerErrorCode.InvalidAuthToken);
+        }
+
+        var userSession = await gameEntryService.EnterGameAsync(jwtToken);
         return GameResponse<UserSession>.Ok(userSession);
     }
 }
