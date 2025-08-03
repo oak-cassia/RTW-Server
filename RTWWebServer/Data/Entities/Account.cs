82293f045969016d69912186cb16091d819ce851
oak_cassia <gnlckswjd1@gmail.com>
2025-08-03 17:01:34 +0900

    feat: Account에 게스트 통합

--- RTWWebServer/Data/Entities/Account.cs
+++ RTWWebServer/Data/Entities/Account.cs
@@ -2,21 +2,32 @@ using RTWWebServer.Enums;
 
 namespace RTWWebServer.Data.Entities;
 
-public class Account(long id, string userName, string email, string password, string salt, UserRole role = UserRole.Normal)
+public class Account
 {
-    private Account() : this(0, string.Empty, string.Empty, string.Empty, string.Empty)
+    public Account(string guid, UserRole role, DateTime createdAt, DateTime updatedAt)
     {
+        Guid = guid;
+        Role = role;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
     }
 
-    public Account(string username, string email, string password, string salt, UserRole role = UserRole.Normal) 
-        : this(0, username, email, password, salt, role)
+    public Account(string email, string password, string salt, UserRole role, DateTime createdAt, DateTime updatedAt)
     {
+        Email = email;
+        Password = password;
+        Salt = salt;
+        Role = role;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
     }
 
-    public long Id { get; init; } = id;
-    public string UserName { get; private set; } = userName;
-    public string Email { get; private set; } = email;
-    public string Password { get; private set; } = password;
-    public string Salt { get; private set; } = salt;
-    public UserRole Role { get; private set; } = role;
+    public long Id { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Salt { get; set; }
+    public string? Guid { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
