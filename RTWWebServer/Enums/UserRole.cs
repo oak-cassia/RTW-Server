3b008bb553f2939b125db9d19897fa8e9b989758
oak_cassia <gnlckswjd1@gmail.com>
2025-08-04 00:22:26 +0900

    refactor: UserRole enum 순서 변경 및 불필요한 확장 메서드 제거

--- RTWWebServer/Enums/UserRole.cs
+++ RTWWebServer/Enums/UserRole.cs
@@ -2,22 +2,7 @@ namespace RTWWebServer.Enums;
 
 public enum UserRole
 {
-    Guest = 0,
-    Normal = 1,
+    Normal = 0,
+    Guest = 1,
     Admin = 2
-}
-
-public static class UserRoleExtensions
-{
-    public static string ToRoleString(this UserRole role)
-    {
-        return role.ToString().ToLowerInvariant();
-    }
-
-    public static UserRole FromRoleString(string roleString)
-    {
-        return Enum.TryParse<UserRole>(roleString, true, out var result)
-            ? result
-            : UserRole.Guest;
-    }
 }
