bd8f581985a9e3bb2f87e3d8a6b7a006e80ecddb
oak_cassia <gnlckswjd1@gmail.com>
2025-08-04 19:04:03 +0900

    refactor: Asp.Net Auth 기능 사용 및 Account ID 기반으로 로직 가독성 향상

--- RTWWebServer/Services/GameEntryService.cs
+++ RTWWebServer/Services/GameEntryService.cs
@@ -2,7 +2,6 @@ using NetworkDefinition.ErrorCode;
 using RTWWebServer.Data.Entities;
 using RTWWebServer.Data.Repositories;
 using RTWWebServer.DTOs;
-using RTWWebServer.Enums;
 using RTWWebServer.Exceptions;
 using RTWWebServer.Providers.Authentication;
 
@@ -10,25 +9,18 @@ namespace RTWWebServer.Services;
 
 public class GameEntryService(
     IGameUnitOfWork unitOfWork,
-    IJwtTokenProvider jwtTokenProvider,
     IUserSessionProvider userSessionProvider,
     ILogger<GameEntryService> logger
 ) : IGameEntryService
 {
-    public async Task<UserSession> EnterGameAsync(string jwtToken)
+    public async Task<UserSession> EnterGameAsync(long accountId)
     {
-        if (string.IsNullOrWhiteSpace(jwtToken))
+        if (accountId <= 0)
         {
-            throw new GameException("JWT token is required", WebServerErrorCode.InvalidAuthToken);
+            throw new GameException("Invalid account ID", WebServerErrorCode.InvalidAuthToken);
         }
 
-        var tokenInfo = jwtTokenProvider.ParseJwtToken(jwtToken);
-        if (tokenInfo?.IsValid != true)
-        {
-            throw new GameException("Invalid JWT token", WebServerErrorCode.InvalidAuthToken);
-        }
-
-        User user = await GetOrCreateUserFromTokenInfoAsync(tokenInfo);
+        User user = await GetOrCreateUserByAccountIdAsync(accountId);
 
         // 새로 생성된 사용자인지 ID로 확인 (ID가 0이면 새로 생성된 사용자)
         if (user.Id == 0)
@@ -42,46 +34,11 @@ public class GameEntryService(
             }
         }
 
-        var userSession = await userSessionProvider.CreateSessionAsync(user.Id, jwtToken);
+        var userSession = await userSessionProvider.CreateSessionAsync(user.Id);
 
         return userSession;
     }
 
-    private async Task<User> GetOrCreateUserFromTokenInfoAsync(JwtTokenInfo tokenInfo)
-    {
-        if (!tokenInfo.UserRole.HasValue)
-        {
-            throw new GameException("User role is missing from JWT token", WebServerErrorCode.InvalidAuthToken);
-        }
-
-        switch (tokenInfo.UserRole.Value)
-        {
-            case UserRole.Normal:
-            {
-                if (string.IsNullOrEmpty(tokenInfo.Email))
-                {
-                    throw new GameException("Failed to extract email from JWT token", WebServerErrorCode.InvalidAuthToken);
-                }
-
-                // Account ID를 통해 User를 조회하도록 변경
-                return await GetOrCreateUserByAccountIdAsync(tokenInfo.AccountId);
-            }
-
-            case UserRole.Guest:
-            {
-                if (!tokenInfo.Guid.HasValue)
-                {
-                    throw new GameException("Failed to extract GUID from JWT token", WebServerErrorCode.InvalidAuthToken);
-                }
-
-                // Account ID를 통해 Guest User를 조회하도록 변경
-                return await GetOrCreateUserByAccountIdAsync(tokenInfo.AccountId);
-            }
-
-            default:
-                throw new GameException("Unsupported user role", WebServerErrorCode.InvalidAuthToken);
-        }
-    }
 
     private async Task<User> GetOrCreateUserByAccountIdAsync(long accountId)
     {
