9b7a8a329de31c24305651834929ec109ebb242f
oak_cassia <gnlckswjd1@gmail.com>
2025-08-15 23:48:22 +0900

    feat: 캐릭터 마스터 데이터, 마스터 데이터의 강타입 클래스

---
+++ RTWWebServer/Data/Entities/CharacterMaster.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RTWWebServer.Data.Entities;
+
+public sealed class CharacterMaster
+{
+    [Range(1, int.MaxValue)]
+    public int Id { get; init; }
+    
+    [Required, MinLength(1)]
+    public string Name { get; init; } = "";
+    
+    [Range(1, 100)]
+    public int Portfolio { get; init; }
+    
+    [Range(1, 100)]
+    public int Development { get; init; }
+    
+    [Range(1, 100)]
+    public int JobSearching { get; init; }
+}
