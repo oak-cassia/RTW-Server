94eada7ce80ff39fdf34cb781f04de7577a78965
oak_cassia <gnlckswjd1@gmail.com>
2025-08-17 13:44:36 +0900

    feat: PlayerCharacter 테이블 추가

--- RTWWebServer/Data/Repositories/GameUnitOfWork.cs
+++ RTWWebServer/Data/Repositories/GameUnitOfWork.cs
@@ -1,6 +1,7 @@
 namespace RTWWebServer.Data.Repositories;
 
-public class GameUnitOfWork(GameDbContext dbContext, IUserRepository userRepository) : BaseUnitOfWork(dbContext), IGameUnitOfWork
+public class GameUnitOfWork(GameDbContext dbContext, IUserRepository userRepository, IPlayerCharacterRepository playerCharacterRepository) : BaseUnitOfWork(dbContext), IGameUnitOfWork
 {
     public IUserRepository UserRepository { get; } = userRepository;
+    public IPlayerCharacterRepository PlayerCharacterRepository { get; } = playerCharacterRepository;
 }
