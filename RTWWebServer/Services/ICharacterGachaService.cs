5a2d72e4fe31e756448a8c48008894392989907c
oak_cassia <gnlckswjd1@gmail.com>
2025-08-18 00:38:38 +0900

    feat: CharacterGachaService 리턴 타입 수정 및 캐릭터 선택 로직 개선

--- RTWWebServer/Services/ICharacterGachaService.cs
+++ RTWWebServer/Services/ICharacterGachaService.cs
@@ -1,9 +1,10 @@
+using RTWWebServer.DTOs;
 using RTWWebServer.DTOs.Response;
 
 namespace RTWWebServer.Services;
 
 public interface ICharacterGachaService
 {
-    Task<CharacterGachaResponse> PerformGachaAsync(long userId, int gachaType, int count);
-    Task<List<PlayerCharacterInfo>> GetPlayerCharactersAsync(long userId);
+    Task<CharacterGachaResult> PerformGachaAsync(long userId, int gachaType, int count);
+    Task<PlayerCharacterInfo[]> GetPlayerCharactersAsync(long userId);
 }
