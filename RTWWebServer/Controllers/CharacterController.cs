00425d45a0a491610c8cc02fd4146af8d2fa08e9
oak_cassia <gnlckswjd1@gmail.com>
2025-08-18 00:37:16 +0900

    feat: 응답 타입 수정

--- RTWWebServer/Controllers/CharacterController.cs
+++ RTWWebServer/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RTWWebServer.DTOs;
 using RTWWebServer.DTOs.Request;
 using RTWWebServer.DTOs.Response;
 using RTWWebServer.Extensions;
@@ -11,18 +12,18 @@ namespace RTWWebServer.Controllers;
 public class CharacterController(ICharacterGachaService characterGachaService) : ControllerBase
 {
     [HttpPost("gacha")]
-    public async Task<GameResponse<CharacterGachaResponse>> PerformGachaAsync([FromBody] CharacterGachaRequest request)
+    public async Task<GameResponse<CharacterGachaResult>> PerformGachaAsync([FromBody] CharacterGachaRequest request)
     {
         var userId = User.GetUserId();
         var result = await characterGachaService.PerformGachaAsync(userId, request.GachaType, request.Count);
-        return GameResponse<CharacterGachaResponse>.Ok(result);
+        return GameResponse<CharacterGachaResult>.Ok(result);
     }
 
     [HttpGet("owned")]
-    public async Task<GameResponse<List<PlayerCharacterInfo>>> GetOwnedCharactersAsync()
+    public async Task<GameResponse<PlayerCharacterInfo[]>> GetOwnedCharactersAsync()
     {
         var userId = User.GetUserId();
         var characters = await characterGachaService.GetPlayerCharactersAsync(userId);
-        return GameResponse<List<PlayerCharacterInfo>>.Ok(characters);
+        return GameResponse<PlayerCharacterInfo[]>.Ok(characters);
     }
 }
