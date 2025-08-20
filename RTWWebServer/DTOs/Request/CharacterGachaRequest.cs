582634d364a73d422f57dd2ffe2831966a424b6e
oak_cassia <gnlckswjd1@gmail.com>
2025-08-20 23:57:37 +0900

    feat: CharacterGachaRequest 검증 추가

--- RTWWebServer/DTOs/Request/CharacterGachaRequest.cs
+++ RTWWebServer/DTOs/Request/CharacterGachaRequest.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RTWWebServer.DTOs.Request;
 
-public record CharacterGachaRequest(int GachaType, int Count);
+public record CharacterGachaRequest(
+    int GachaType,
+    [Range(1, int.MaxValue, ErrorMessage = "Count must be greater than 0")]
+    int Count);
