1e7ea2db72cd8d1993b37d85001ff55997c4d62a
oak_cassia <gnlckswjd1@gmail.com>
2026-02-01 22:54:38 +0900

    feat: chat packet 추가

--- RTWServer/Game/Packet/GamePacketFactory.cs
+++ RTWServer/Game/Packet/GamePacketFactory.cs
@@ -14,7 +14,14 @@ public class GamePacketFactory : IPacketFactory
         {
             PacketId.EchoMessage => new ProtoPacket(packetId, EchoMessage.Parser.ParseFrom(payloadBytes)),
             PacketId.CAuthToken => new ProtoPacket(packetId, CAuthToken.Parser.ParseFrom(payloadBytes)),
+            PacketId.CChat => new ProtoPacket(packetId, CChat.Parser.ParseFrom(payloadBytes)),
+            PacketId.CChatJoin => new ProtoPacket(packetId, CChatJoin.Parser.ParseFrom(payloadBytes)),
+            PacketId.CChatLeave => new ProtoPacket(packetId, CChatLeave.Parser.ParseFrom(payloadBytes)),
+            PacketId.CChatChat => new ProtoPacket(packetId, CChatChat.Parser.ParseFrom(payloadBytes)),
             PacketId.SAuthResult => new ProtoPacket(packetId, SAuthResult.Parser.ParseFrom(payloadBytes)),
+            PacketId.SChat => new ProtoPacket(packetId, SChat.Parser.ParseFrom(payloadBytes)),
+            PacketId.SChatJoinResult => new ProtoPacket(packetId, SChatJoinResult.Parser.ParseFrom(payloadBytes)),
+            PacketId.SChatLeaveResult => new ProtoPacket(packetId, SChatLeaveResult.Parser.ParseFrom(payloadBytes)),
             _ => throw new ArgumentOutOfRangeException(nameof(packetId), packetId, null)
         };
     }
